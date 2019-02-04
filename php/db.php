b882a323e0b9152dd6b9b416c3716577653235bd
Dana Prakoso <danaossoftware@gmail.com>
2019-02-05 03:26:02 +0700

    commit

--- php/db.php
+++ php/db.php
@@ -1,3 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
 $c = new mysqli("localhost", "u554752604_admin", "HelloWorld123");
 $c->select_db("u554752604_ddt");
